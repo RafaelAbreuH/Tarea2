45fc39b0920b76eec8731b85185d31cefb1b301a
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2020-01-22 01:54:19 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using GenerosBlazorApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerosBlazorApp.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Genero> Genero { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server = (local); Database = Tarea2Generos; Trusted_Connection = True;");
+        }
+    }
+}
